d704c2802563e695fbfa03afe98f6d941b8e0aa6
Mark Sherriff <sherriff@virginia.edu>
2021-09-28 11:41:54 -0400

    change!

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the index view for the app called 'myapp' in the project called 'buildexample'")
+    return HttpResponse("This is the index view for the app called 'myapp' in the project called 'buildexample' asdfasfafsd")
